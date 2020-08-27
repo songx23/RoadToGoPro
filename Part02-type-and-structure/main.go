e79e4430c7a2188abda8f94e3f9b571d051220a7
Song Xue <song.xue@digio.com.au>
2020-08-27 22:47:23 +1000

    Add code for primitive types

--- Part02-type-and-structure/main.go
+++ Part02-type-and-structure/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("type and structure")
+	// primitive type: boolean
+	var isGoAwesome bool
+	isGoAwesome = false // just kidding!
+	isGoAwesome = true
+	fmt.Println(isGoAwesome)
+
+	// primitive type: integer
+	var integer int
+	integer = -10
+	integer = 10
+	fmt.Println(integer)
+
+	var followerCount uint32
+	followerCount = 10000
+	// overflow error
+	// followerCount = -1000
+	fmt.Println(followerCount)
+
+	// primitive type: float
+	var balance float32
+	balance = 10000.01
+	fmt.Println(balance)
+
+	// primitive type: string
+	var text string
+	text = "Go is fun."
+	fmt.Println(text)
+
+	// an easier way to declare variables
+	// import "reflect"
+	isGoAwesomeNew := true
+	fmt.Println(reflect.TypeOf(isGoAwesomeNew))
+	integerNew := 100
+	fmt.Println(reflect.TypeOf(integerNew))
+	floatNew := 100.1
+	fmt.Println(reflect.TypeOf(floatNew))
+	textNew := "Go is fun"
+	fmt.Println(reflect.TypeOf(textNew))
 }
