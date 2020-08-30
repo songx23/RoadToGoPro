dbb9d86a39b30d1f6c5fc36f58a454ec3fe4ad01
Song Xue <song.xue@digio.com.au>
2020-08-30 15:02:50 +1000

    Add sample code for map, struct, interface

--- Part02-type-and-structure/main.go
+++ Part02-type-and-structure/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 func main() {
@@ -74,7 +75,7 @@ func main() {
 	// to get avocado from my shopping list, I need to find its index
 	avocado := shoppingList[1]
 	// fmt package can do better than this but let's stick with Println for now
-	fmt.Println("Smash ", avocado, "\n")
+	fmt.Println("Smash ", avocado)
 
 	// composite type: slice
 	// slice can stuff as many kinds of bread as you can imagine
@@ -117,4 +118,118 @@ func main() {
 		6000: "Perth",
 	}
 	fmt.Println(postcodesInline)
+
+	// access value in map using key
+	melbourne := postcodes[3000]
+	fmt.Println(melbourne) // output: Melbourne
+
+	// access non-existing key
+	darwin := postcodes[8000]
+	fmt.Println("darwin is empty string? ", darwin == "") // darwin: empty string
+
+	// check key existed or not
+	darwin, ok := postcodes[8000]
+	fmt.Println("is key present in map? ", ok)            // output: false
+	fmt.Println("darwin is empty string? ", darwin == "") // darwin: empty string
+
+	// add key value pair
+	postcodes[4000] = "Brisbane"
+	fmt.Println(postcodes) // should see a new pair in map
+
+	// delete key value pair
+	delete(postcodes, 4000)
+	fmt.Println(postcodes) // should see 4000:Brisbane is removed from map
+
+	// Composite type: struct
+	// declare an account
+	var account1 Account
+	account1.BSB = 123456
+	account1.AccountNumber = 12345678
+	account1.AccountType = "Joint Account"
+	account1.Owners = []Owner{
+		{
+			FirstName:   "Jon",
+			LastName:    "Snow",
+			PhoneNumber: "0400100200",
+			Email:       "jon.snow@got.com",
+		},
+		{
+			FirstName:   "Daenerys",
+			LastName:    "Stormborn",
+			PhoneNumber: "0400300400",
+			Email:       "mother.of.dragons@got.com",
+		},
+	}
+	fmt.Printf("%+v\n", account1)
+
+	// another way to declare an account inline
+	account2 := Account{
+		BSB:           123456,
+		AccountNumber: 12345678,
+		AccountType:   "Joint Account",
+		Owners: []Owner{
+			{
+				FirstName:   "Jon",
+				LastName:    "Snow",
+				PhoneNumber: "0400100200",
+				Email:       "jon.snow@got.com",
+			},
+			{
+				FirstName:   "Daenerys",
+				LastName:    "Stormborn",
+				PhoneNumber: "0400300400",
+				Email:       "mother.of.dragons@got.com",
+			},
+		},
+	}
+	fmt.Printf("%+v\n", account2)
+
+	// Composite type: interface
+	// declare a variable with interface type
+	var poodle Animal
+	poodle = Dog{Name: "Bolt"}
+	fmt.Println(poodle.Eat())
+
+	// Type conversion
+	convertInt := 1
+	convertFloat := float32(convertInt)
+	fmt.Println(convertFloat, reflect.TypeOf(convertFloat))
+	convertAnotherFloat := 1.8
+	convertInt = int(convertAnotherFloat)
+	fmt.Println(convertInt, reflect.TypeOf(convertInt))
+
+	// parse string to boolean
+	convertBool, err := strconv.ParseBool("true")
+	fmt.Println(err == nil, convertBool, reflect.TypeOf(convertBool)) // output: true true bool
+	// convert string to int
+	strConvertInt, err := strconv.Atoi("-42")
+	fmt.Println(err == nil, strConvertInt, reflect.TypeOf(strConvertInt)) // output: true -42 int
+}
+
+// Composite type: struct
+type Owner struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Email       string
+}
+type Account struct {
+	BSB           int
+	AccountNumber int64
+	AccountType   string
+	Owners        []Owner
+}
+
+// Composite type: interface
+type Animal interface {
+	// function sets
+	Eat() string
+}
+
+type Dog struct {
+	Name string
+}
+
+func (d Dog) Eat() string {
+	return fmt.Sprintf("%s eats meat", d.Name)
 }
