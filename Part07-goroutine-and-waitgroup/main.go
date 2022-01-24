cf2c0fd7f7fe3253fe150023b4ffcb408a265b97
Song Xue <songxue.aws@gmail.com>
2022-01-24 21:40:07 +1100

    add example code

--- Part07-goroutine-and-waitgroup/main.go
+++ Part07-goroutine-and-waitgroup/main.go
@@ -1,1 +1,40 @@
-package Part07_goroutine_and_waitgroup
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	// sequential processing
+	seqStart := time.Now()
+	for i := 0; i < 6; i++ {
+		hardCalculation(i)
+	}
+	seqDur := time.Since(seqStart).Milliseconds()
+	fmt.Printf("Total time spent: %d ms\n", seqDur)
+	// async processing
+	asyncStart := time.Now()
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 6; i++ {
+		wg.Add(1)
+		go hardCalculationAsync(wg, i)
+	}
+	wg.Wait()
+	asyncDur := time.Since(asyncStart).Milliseconds()
+	fmt.Printf("Total time spent: %d ms\n", asyncDur)
+}
+
+func hardCalculation(input int) {
+	// pretending I'm working on super hard math problems
+	time.Sleep(10 * time.Second)
+	fmt.Printf("math problem solved for input: %d.\n", input)
+}
+
+func hardCalculationAsync(wg *sync.WaitGroup, input int) {
+	// pretending I'm working on super hard math problems
+	defer wg.Done()
+	time.Sleep(1 * time.Second)
+	fmt.Printf("math problem solved for input: %d.\n", input)
+}
