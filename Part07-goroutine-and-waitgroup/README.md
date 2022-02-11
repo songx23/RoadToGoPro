209936b9538d5e95cda5267ec1ba153e3e382576
Song Xue <songxue.aws@gmail.com>
2022-02-11 12:18:22 +1100

    add readme and test

--- Part07-goroutine-and-waitgroup/README.md
+++ Part07-goroutine-and-waitgroup/README.md
@@ -1,6 +1,6 @@
 # Road to Go Pro — Goroutines and Waitgroups
 
-Please read the tutorial on [Medium](https://medium.com/@songx).
+Please read the tutorial on [Medium](https://levelup.gitconnected.com/road-to-go-pro-async-part-1-goroutine-waitgroup-e915accab659).
 
 ## Run the example code
 
--- Part08-channel-and-lock/main_test.go
+++ Part08-channel-and-lock/main_test.go
@@ -5,10 +5,14 @@ import (
 	"testing"
 )
 
-func BenchmarkWorker_ProcessSetupTest_1(b *testing.B)    { benchmarkProcess(1, b) }
-func BenchmarkWorker_ProcessSetupTest_10(b *testing.B)   { benchmarkProcess(10, b) }
-func BenchmarkWorker_ProcessSetupTest_100(b *testing.B)  { benchmarkProcess(100, b) }
-func BenchmarkWorker_ProcessSetupTest_1000(b *testing.B) { benchmarkProcess(1000, b) }
+func BenchmarkWorker_ProcessSetupTest_1(b *testing.B)      { benchmarkProcess(1, b) }
+func BenchmarkWorker_ProcessSetupTest_10(b *testing.B)     { benchmarkProcess(10, b) }
+func BenchmarkWorker_ProcessSetupTest_100(b *testing.B)    { benchmarkProcess(100, b) }
+func BenchmarkWorker_ProcessSetupTest_1000(b *testing.B)   { benchmarkProcess(1000, b) }
+func BenchmarkWorker_ProcessSetupTest_10000(b *testing.B)  { benchmarkProcess(10000, b) }
+func BenchmarkWorker_ProcessSetupTest_100000(b *testing.B) { benchmarkProcess(100000, b) }
+func BenchmarkWorker_ProcessSetupTest_200000(b *testing.B) { benchmarkProcess(200000, b) }
+func BenchmarkWorker_ProcessSetupTest_300000(b *testing.B) { benchmarkProcess(300000, b) }
 
 func benchmarkProcess(workerCount int, b *testing.B) {
 	wg := &sync.WaitGroup{}
