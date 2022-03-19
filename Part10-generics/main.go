6941ef59e3bc8cdaeacea3d50371df4d1a8806dd
Song Xue <songxue.aws@gmail.com>
2022-03-19 14:50:11 +1100

    clean up code

--- Part10-generics/main.go
+++ Part10-generics/main.go
@@ -3,10 +3,11 @@ package main
 import "net/http"
 
 func main() {
-	m := Magic{"Flying spell", []string{"Wingardium", "Leviosa"}}
-
+	m1 := Magic{"Protection Spell", []string{"Expecto", "Patronum"}}
+	m2 := Magic{"Petrol price is crazy", []string{"Expensive", "Petroleum"}}
 	err := CustomError{http.StatusNotImplemented, "Panic! Your code is on fire!"}
 
-	GenericsToString(&m)
+	GenericsToString(&m1)
+	GenericsToString(&m2)
 	GenericsToString(&err)
 }
